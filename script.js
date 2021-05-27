db7d4aaafe9283ef0a59d009cd313692e47f9e70
Ncesecrede <francescopm95@gmail.com>
2021-05-27 12:57:00 +0100

    passwordGenerator

---
+++ script.js
@@ -0,0 +1,5 @@
+var nome = prompt("Inserisci nome")
+var cognome = prompt("Inserisci cognome")
+var colore = prompt("Inserisci colore preferito")
+
+document.getElementById("datiUtente").innerHTML = nome + cognome + colore + "21";
